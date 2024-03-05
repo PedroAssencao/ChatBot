0d9a44f94cacea25f3036d8cc56dc102729dc354
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2024-03-05 20:39:26 -0300

    a

---
+++ Chatbot.Solution/Chatbot.API/Controllers/TesteController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chatbot.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/teste")]
+        public int testre() 
+        {
+
+            return 1 + 2;
+        
+        }
+
+        [HttpPost("/hook")]
+        public async Task<IActionResult> HandleWebhookAsync([FromBody] dynamic requestBody)
+        {
+           
+            return Ok(); 
+        }
+
+        //Usar Esse Codigo Na Validação para Não dar error
+        //[HttpGet("/hook")]
+        //public IActionResult HandleWebhook([FromQuery(Name = "hub.challenge")] string hubChallenge)
+        //{
+        //    return Ok(hubChallenge);
+        //}
+    }
+}
