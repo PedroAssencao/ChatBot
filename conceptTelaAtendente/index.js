cb0b12f0b10a91b1b88fcd2a67121d65094122db
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2024-05-08 12:22:57 -0300

    a

--- conceptTelaAtendente/index.js
+++ conceptTelaAtendente/index.js
@@ -4,12 +4,12 @@ function entrarChat() {
     document.getElementById('navbar').style.display = 'none'
     document.getElementById('sidebar').style.display = 'none'
     document.getElementById('containerMensagens').style.display = 'none'
-    document.getElementById('containerChats').style.display = 'block'
+    document.getElementById('containerChats').style.display = 'flex'
 }
 
 function voltarChat() {
     document.getElementById('navbar').style.display = 'block'
-    document.getElementById('sidebar').style.display = 'block'
+    document.getElementById('sidebar').style.display = 'flex'
     document.getElementById('containerMensagens').style.display = 'block'
     document.getElementById('containerChats').style.display = 'none'
 }
@@ -27,9 +27,35 @@ function verificarLargura(bool) {
     } else {
         // Caso contrário, a largura da tela é menor que 991px
         // Neste exemplo, não fazemos nada ou podemos desativar a função
-        console.log("A largura da tela é menor que 992px. A função não será executada.");
+
     }
 }
 
 // Registre um ouvinte de evento para verificar a largura da tela quando a janela é redimensionada
 window.addEventListener('resize', verificarLargura(false));
+
+
+function VerficarAltura() {
+    var larguraJanela = window.innerHeight;
+    console.log(larguraJanela);
+
+    if (larguraJanela < 559) {
+        document.querySelector('.ConteudoChat').style.maxHeight = "65vh";
+        document.querySelector('.ConteudoChat').style.height = "65vh";
+    } else if (larguraJanela < 670) {
+        document.querySelector('.ConteudoChat').style.maxHeight = "67vh";
+        document.querySelector('.ConteudoChat').style.height = "67vh";
+    } else if (larguraJanela < 768) {
+        document.querySelector('.ConteudoChat').style.maxHeight = "70vh";
+        document.querySelector('.ConteudoChat').style.height = "70vh";
+    } else if (larguraJanela < 874) {
+        document.querySelector('.ConteudoChat').style.maxHeight = "74vh";
+        document.querySelector('.ConteudoChat').style.height = "74vh";
+    } else {
+        document.querySelector('.ConteudoChat').style.maxHeight = "77vh";
+        document.querySelector('.ConteudoChat').style.height = "77vh";
+    }
+}
+
+window.addEventListener('resize', VerficarAltura);
+VerficarAltura();
