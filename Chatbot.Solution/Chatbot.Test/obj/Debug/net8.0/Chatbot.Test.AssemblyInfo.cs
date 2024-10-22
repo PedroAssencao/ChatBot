9a82beaeede71386f31f9eb2d620271b876dffa2
Miguel-Margi <115711899+PedroAssencao@users.noreply.github.com>
2024-10-22 16:22:00 -0300

    ainda nao resolvido

--- Chatbot.Solution/Chatbot.Test/obj/Debug/net8.0/Chatbot.Test.AssemblyInfo.cs
+++ Chatbot.Solution/Chatbot.Test/obj/Debug/net8.0/Chatbot.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chatbot.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d6c271da5caafeb2a8f512bd1e8f8d14132c69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcc535bc6ce3faa555d67aaf8a10033bfc001dfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chatbot.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chatbot.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
